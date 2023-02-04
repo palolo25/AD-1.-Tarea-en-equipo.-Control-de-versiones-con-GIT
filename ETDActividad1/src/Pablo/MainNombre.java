8820488947815972e0de942d00d573b8a1013116
pablo segura barranco <89455065+palolo25@users.noreply.github.com>
2023-02-04 12:17:21 +0100

    Paquete pablo

---
+++ ETDActividad1/src/Pablo/MainNombre.java
@@ -0,0 +1,13 @@
+package Pablo;
+
+public class MainNombre {
+
+	public static void main(String[] args) {
+		
+		Nombre n1 = new Nombre();
+		
+		n1.mostrarNombre();
+
+	}
+
+}
---
+++ ETDActividad1/src/Pablo/Nombre.java
@@ -0,0 +1,13 @@
+package Pablo;
+
+public class Nombre {
+
+	String nombre = "Pablo";
+	
+	
+	
+	public void mostrarNombre() {
+		
+		System.out.println("Mi nombre es "+nombre);
+	}
+}
